aed418c16221a49f4a906320c4762979c2cdaa82
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-11 23:57:04 -0300

    feat: configured multer

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 
 import './database/connection';
+
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use(routes);
 

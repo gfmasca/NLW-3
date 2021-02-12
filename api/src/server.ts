d78008b9e80d607283a5874fc4f5c03945d8467b
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-12 00:52:38 -0300

    feat: implemented cross origin with cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,6 +3,7 @@ import express, {
 } from 'express';
 
 import './database/connection';
+import cors from 'cors';
 import 'express-async-errors';
 import { MulterError } from 'multer';
 import { ValidationError } from 'yup';
@@ -15,6 +16,7 @@ import IValidationError from './errors/IValidationError';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use(routes);

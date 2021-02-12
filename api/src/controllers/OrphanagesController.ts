5fb11efb30ce3122a890f7c803be769e3dba1c07
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-11 21:04:41 -0300

    feat: orphanage creating feature

---
+++ api/src/controllers/OrphanagesController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../database/entities/Orphanage';
+
+export default class OrphanagesController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = req.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+
+    await orphanageRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  }
+}

63d66107e06a97644a0f5df0d81ea8521fb9dadd
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-16 16:24:38 -0300

    fix: removed eslint problems

--- si/src/pages/Landing.tsx
+++ si/src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 import {
-  Route, Switch, BrowserRouter, Link,
+  Link,
 } from 'react-router-dom';
 
 import logoImg from '../images/logo.svg';
